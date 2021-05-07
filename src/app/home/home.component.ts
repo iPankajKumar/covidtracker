c52a7dabff1acbab27b549d75a0e26bc0759f5bb
Pankaj Kumar <pkumar@scan-it.com.sg>
2021-05-07 20:16:24 +0530

    Refactoring of code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, TemplateRef, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import * as moment from 'moment';
 import * as AllIndiaPincodes from '../../assets/json/AllIndiaPincodes.json';
-import { debounceTime } from 'rxjs/operators';
 import { HttpClient } from "@angular/common/http";
 import { BsModalService, BsModalRef, ModalDirective } from 'ngx-bootstrap/modal';
 import { ToastrService } from 'ngx-toastr';
@@ -22,7 +21,7 @@ export class HomeComponent implements OnInit {
   config = {
     animated: true
   };
-  constructor(private httpClient :HttpClient,private modalService: BsModalService, private toastr: ToastrService) {
+  constructor(private httpClient: HttpClient, private modalService: BsModalService, private toastr: ToastrService) {
     this.timerCount(5);
   }
   dateValueSingle: Date;
@@ -30,11 +29,12 @@ export class HomeComponent implements OnInit {
   dateValueTo: Date;
   trialCounter = 1;
   dateArrray = [];
-  vaccinationSlotResponse:any=[];
+  vaccinationSlotCurrentResponse: any = [];
+  vaccinationSlotAllResponse: any = [];
   timer = 0;
   sleepNow = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
-  vaccinationSlotUrl = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=";
-  ageCategory: number =0;
+  vaccinationSlotUrlByPinCode = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=";
+  ageCategory: number = 0;
 
 
   isModalShown = true;
@@ -44,7 +44,7 @@ export class HomeComponent implements OnInit {
   hideModal() {
     this.autoShownModal.hide();
   }
-  hideModalSearch(){
+  hideModalSearch() {
     this.searchModal.hide();
   }
   openModalSearch() {
@@ -52,7 +52,7 @@ export class HomeComponent implements OnInit {
     this.autoShownModal.hide();
   }
   searchSlotModal() {
-    
+
     this.searchModal.show();
   }
   ngOnInit() {
@@ -60,7 +60,7 @@ export class HomeComponent implements OnInit {
     var indianState = [...new Set(AllIndiaPincodes['default'].map(item => item.STATENAME))];
     //Sort states alphabetically.
     this.indianStates = indianState.sort((a: any, b: any) => a.localeCompare(b));
-    
+
   }
 
   fetchCities(stateName: string) {
@@ -86,62 +86,62 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  showToasterMessage(title: string, message: string, type: string){
+  showToasterMessage(title: string, message: string, type: string) {
     switch (type) {
       case 'success':
-        this.toastr.success(title,message,{
+        this.toastr.success(title, message, {
           timeOut: 10000,
-          positionClass :'toast-top-center'
+          positionClass: 'toast-top-center'
         });
         break;
 
-        case 'warning':
-          this.toastr.warning(title,message,{
-            timeOut: 10000,
-            positionClass :'toast-top-center'
-          });
-          break;
+      case 'warning':
+        this.toastr.warning(title, message, {
+          timeOut: 10000,
+          positionClass: 'toast-top-center'
+        });
+        break;
 
-          case 'info':
-            this.toastr.info(title,message,{
-              timeOut: 10000,
-              positionClass :'toast-top-center'
-            });
-            break;
+      case 'info':
+        this.toastr.info(title, message, {
+          timeOut: 10000,
+          positionClass: 'toast-top-center'
+        });
+        break;
 
-            case 'error':
-            this.toastr.error(title,message,{
-              timeOut: 10000,
-              positionClass :'toast-top-center'
-            });
-            break;
-    
+      case 'error':
+        this.toastr.error(title, message, {
+          timeOut: 10000,
+          positionClass: 'toast-top-center'
+        });
+        break;
+
       default:
         break;
     }
-   
+
 
   }
 
-   async slotsByPincodeAndDate() {
+  async slotsByPincodeAndDate() {
 
-    
+
     this.isSearchModalShown = false;
     console.log("Trial counter", this.trialCounter++);
-    this.dateArrray=[];
+    this.dateArrray = [];
     this.dateArrray.push(this.dateValueSingle);
     this.showToasterMessage('', 'You have done your part, we will take it from here. Sit back and relax while we crunch some numbers and show you all the available slots within your preferences.', 'info');
     for (let pIndex = 0; pIndex < this.selectedCodes.length; pIndex++) {
       for (let dIndex = 0; dIndex < this.dateArrray.length; dIndex++) {
-        let url = this.vaccinationSlotUrl +  this.selectedCodes[pIndex]['pinCode'] + "&date=" + moment(this.dateArrray[dIndex]).format("DD-MM-YYYY");
-        
+        let url = this.vaccinationSlotUrlByPinCode + this.selectedCodes[pIndex]['pinCode'] + "&date=" + moment(this.dateArrray[dIndex]).format("DD-MM-YYYY");
+
         await this.sleepNow(10000);
-       let subscription=  this.httpClient.get(url).subscribe((data)=>{
-          this.vaccinationSlotResponse =  data ;//JSON.parse(data);
+        let subscription = this.httpClient.get(url).subscribe((data) => {
+          this.vaccinationSlotCurrentResponse = data;//JSON.parse(data);
           this.validCenters();
           subscription.unsubscribe();
-         
-        },(error)=>{
+
+        }, (error) => {
           this.showToasterMessage('', 'We encountered an error, but it is not your fault, please try again later.', 'error');
         });
       }
@@ -152,38 +152,34 @@ export class HomeComponent implements OnInit {
     }, 3000000);
   }
 
-  validCenters(){
-    
-    if(this.vaccinationSlotResponse && this.vaccinationSlotResponse.centers.length > 0){
-      for (let centre in this.vaccinationSlotResponse.centers) {
-        this.vaccinationSlotResponse.centers[centre].isValidCentre =false;
-        this.vaccinationSlotResponse.centers[centre].availableSlotsCount = 0;
-        for (let session in this.vaccinationSlotResponse.centers[centre].sessions) {
-         
-          if(this.ageCategory==1){
-            if (this.vaccinationSlotResponse.centers[centre].sessions[session].min_age_limit > 44 && this.vaccinationSlotResponse.centers[centre].sessions[session].available_capacity > 0) 
-          
-            {
-              this.vaccinationSlotResponse.centers[centre].isValidCentre = true;
-              this.vaccinationSlotResponse.centers[centre].availableSlotsCount++;
-             
+  validCenters() {
+
+    if (this.vaccinationSlotCurrentResponse && this.vaccinationSlotCurrentResponse.centers.length > 0) {
+      for (let centre in this.vaccinationSlotCurrentResponse.centers) {
+        this.vaccinationSlotCurrentResponse.centers[centre].isValidCentre = false;
+        this.vaccinationSlotCurrentResponse.centers[centre].availableSlotsCount = 0;
+        for (let session in this.vaccinationSlotCurrentResponse.centers[centre].sessions) {
+
+          if (this.ageCategory == 1) {
+            if (this.vaccinationSlotCurrentResponse.centers[centre].sessions[session].min_age_limit > 44 && this.vaccinationSlotCurrentResponse.centers[centre].sessions[session].available_capacity > 0) {
+              this.vaccinationSlotCurrentResponse.centers[centre].isValidCentre = true;
+              this.vaccinationSlotCurrentResponse.centers[centre].availableSlotsCount++;
+
             }
-          }else if(this.ageCategory==0){
-            if (this.vaccinationSlotResponse.centers[centre].sessions[session].min_age_limit >17 && this.vaccinationSlotResponse.centers[centre].sessions[session].available_capacity > 0) 
-          
-            {
-              this.vaccinationSlotResponse.centers[centre].isValidCentre = true;
-              this.vaccinationSlotResponse.centers[centre].availableSlotsCount++;
-             
-              
+          } else if (this.ageCategory == 0) {
+            if (this.vaccinationSlotCurrentResponse.centers[centre].sessions[session].min_age_limit > 17 && this.vaccinationSlotCurrentResponse.centers[centre].sessions[session].available_capacity > 0) {
+              this.vaccinationSlotCurrentResponse.centers[centre].isValidCentre = true;
+              this.vaccinationSlotCurrentResponse.centers[centre].availableSlotsCount++;
+
+
             }
           }
 
-         
+
         }
       }
     }
-    console.log("data->>>>>>>>>>> ",this.vaccinationSlotResponse);
+    console.log("data->>>>>>>>>>> ", this.vaccinationSlotCurrentResponse);
   }
 
   display: any;
